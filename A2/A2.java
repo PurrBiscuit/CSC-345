25340767e3fed6905d351e27df480f6762be2aef
Purrbiscuit <spurr@articulate.com>
2022-10-10 20:08:33 -0400

    add basic lexeme finding code

--- A2/A2.java
+++ A2/A2.java
@@ -1,8 +1,8 @@
 /**
  * CSC 345-01 Assignment #2
- * 
- * On my honor, Steve Purr, this assignment is my own work.  
- * I, Steve Purr, will follow the instructor's rules and processes 
+ *
+ * On my honor, Steve Purr, this assignment is my own work.
+ * I, Steve Purr, will follow the instructor's rules and processes
  * related to academic integrity as directed in the course syllabus.
  *
  */
@@ -12,30 +12,104 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 // Students -- Add your import declarations here
+import java.util.HashMap;
 
 public class A2 {
-	
-	// Students -- Add your constants here
-	
-	public static void main(String[] args) {
-		try {
-			// Do NOT make any changes to the following TWO lines
-			File file = new File(args[0]);		
-			Scanner sc = new Scanner(file);		//*** Do not make any other Scanners ***//
-			
-			// Students -- Your code and methods calls go here
-			
-			
-			
-			
-			sc.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("ERROR - cannot open front.in \n");
-		}
-	}
-	
-	// Students -- Add your method declarations here
+  // Students -- Add your constants here
+  private static final String defaultLexeme = "";
 
+  // Character classes
+  private static final int
+    EOF = -1,
+    LETTER = 0,
+    DIGIT = 1,
+    UNKNOWN = 99;
 
+  // Token codes
+  private static final int
+    FLOATDCL = 0,
+    INTDCL = 1,
+    PRINT = 2,
+    ID = 3,
+    ASSIGN = 4,
+    PLUS = 5,
+    MINUS = 6,
+    INUM = 7,
+    FNUM = 8;
 
+  private static char nextChar;
+  private static HashMap<Character, Integer> reservedLexemes = new HashMap<Character, Integer>();
+  private static int charClass, currentLineIndex = 0, nextToken;
+  private static String currentLine, lexeme;
+
+  public static void main(String[] args) {
+    try {
+      // Do NOT make any changes to the following TWO lines
+      File file = new File(args[0]);
+      Scanner sc = new Scanner(file);   //*** Do not make any other Scanners ***//
+      currentLine = sc.nextLine();
+
+      // Students -- Your code and methods calls go here
+      reservedLexemes.put('f', FLOATDCL);
+      reservedLexemes.put('i', INTDCL);
+      reservedLexemes.put('p', PRINT);
+      reservedLexemes.put('=', ASSIGN);
+      reservedLexemes.put('+', PLUS);
+      reservedLexemes.put('-', MINUS);
+
+      getChar();
+      getLexeme();
+
+      sc.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("ERROR - cannot open front.in \n");
+    }
+  }
+
+  // Students -- Add your method declarations here
+  private static void addChar() {
+    lexeme+=nextChar;
+  }
+
+  private static void getChar() {
+    try {
+      nextChar = currentLine.charAt(currentLineIndex);
+
+      charClass = Character.isDigit(nextChar) ? DIGIT : UNKNOWN;
+
+      currentLineIndex++;
+    } catch (StringIndexOutOfBoundsException e) {
+      charClass = EOF;
+    }
+  }
+
+  private static void getLexeme() {
+    lexeme = "";
+
+    skipWhitespace();
+
+    switch (charClass) {
+      case DIGIT:
+        do {
+          addChar();
+          getChar();
+        } while (charClass == DIGIT || nextChar == '.');
+
+        nextToken = lexeme.contains(".") ? FNUM : INUM;
+        break;
+      default:
+        nextToken = reservedLexemes.getOrDefault(nextChar, ID);
+        lexeme = Character.toString(nextChar);
+        getChar();
+        break;
+    }
+
+    System.out.println("Next token is: " + nextToken + ", Next lexeme is " + lexeme);
+  }
+
+  private static void skipWhitespace() {
+    while (Character.isWhitespace(nextChar)) {
+      getChar();
+    }
+  }
 }
